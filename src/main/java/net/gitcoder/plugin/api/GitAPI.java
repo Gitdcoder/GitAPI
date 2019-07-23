f9124cc590789111440c12192c1c494518fb38f7
Sergey Yurchenko <worcanyts@yandex.ru>
2019-07-24 01:47:32 +0300

    ** ТВОРИМ ДОБРО <>.<> **

--- src/main/java/net/gitcoder/plugin/api/GitAPI.java
+++ src/main/java/net/gitcoder/plugin/api/GitAPI.java
@@ -1,9 +1,10 @@
-package net.gitcoder.api;
+package net.gitcoder.plugin.api;
 
 /**
- * Developed by James_TheMan.
+ * Developed by GitCoder.
  * You may not change this code or change copyright.
  * The author is responsible for this code.
+ * All right's is reserved.
  */
 public class GitAPI {
 }
---
+++ src/main/java/net/gitcoder/plugin/api/bukkit/module/board/Board.java
@@ -0,0 +1,10 @@
+package net.gitcoder.plugin.api.bukkit.module.board;
+
+/**
+ * Developed by GitCoder.
+ * You may not change this code or change copyright.
+ * The author is responsible for this code.
+ * All right's is reserved.
+ */
+public class Board {
+}
